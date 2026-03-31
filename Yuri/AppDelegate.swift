92d31d172cacdd2a7e1ddfc99d5713d03cbdea26
pignuante <hanyul.ryu@hanyul.xyz>
2026-03-31 09:54:12 +0900

    feat: add menu bar shell and accessibility settings

--- Yuri/AppDelegate.swift
+++ Yuri/AppDelegate.swift
@@ -8,16 +8,162 @@
 import Cocoa
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
+final class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
+    private var statusItem: NSStatusItem?
+    private var settingsWindowController: NSWindowController?
+    private let permissionStatusMenuItem = NSMenuItem()
+    private let openAccessibilitySettingsMenuItem = NSMenuItem(
+        title: "Open Accessibility Settings…",
+        action: #selector(openAccessibilitySettings(_:)),
+        keyEquivalent: ""
+    )
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        configureActivationPolicy()
+        configureStatusItem()
+        refreshPermissionState()
+        debugShowSettingsOnLaunchIfNeeded()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleDidBecomeActive(_:)),
+            name: NSApplication.didBecomeActiveNotification,
+            object: nil
+        )
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        true
+    }
+
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        openSettings(sender)
         return true
+    }
+
+    func menuWillOpen(_ menu: NSMenu) {
+        refreshPermissionState()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc private func openSettings(_ sender: Any?) {
+        guard let controller = settingsWindowController ?? makeSettingsWindowController() else {
+            NSSound.beep()
+            return
+        }
+
+        settingsWindowController = controller
+        controller.showWindow(sender)
+        controller.window?.makeKeyAndOrderFront(sender)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
+    @objc private func openAccessibilitySettings(_ sender: Any?) {
+        _ = AccessibilityPermissionService.requestPrompt()
+
+        guard AccessibilityPermissionService.openSystemSettings() else {
+            NSSound.beep()
+            return
+        }
+    }
+
+    @objc private func handleDidBecomeActive(_ notification: Notification) {
+        refreshPermissionState()
+    }
+
+    @objc private func quit(_ sender: Any?) {
+        NSApp.terminate(sender)
+    }
+
+    private func configureStatusItem() {
+        let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        let button = item.button
+
+        button?.image = nil
+        button?.title = "Yuri"
+        button?.toolTip = "Yuri"
+        item.menu = makeStatusMenu()
+        item.isVisible = true
+
+        statusItem = item
+        NSLog("Yuri status item created. button=%@", String(describing: button))
+    }
+
+    private func makeStatusMenu() -> NSMenu {
+        let menu = NSMenu()
+        menu.delegate = self
+
+        permissionStatusMenuItem.isEnabled = false
+        menu.addItem(permissionStatusMenuItem)
+
+        openAccessibilitySettingsMenuItem.target = self
+        menu.addItem(openAccessibilitySettingsMenuItem)
+
+        menu.addItem(.separator())
+
+        let settingsItem = NSMenuItem(
+            title: "Open Settings…",
+            action: #selector(openSettings(_:)),
+            keyEquivalent: ","
+        )
+        settingsItem.target = self
+        menu.addItem(settingsItem)
+
+        menu.addItem(.separator())
+
+        let quitItem = NSMenuItem(
+            title: "Quit Yuri",
+            action: #selector(quit(_:)),
+            keyEquivalent: "q"
+        )
+        quitItem.target = self
+        menu.addItem(quitItem)
+
+        return menu
+    }
+
+    private func makeSettingsWindowController() -> NSWindowController? {
+        let viewController = ViewController()
+        let window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 460, height: 260),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.center()
+        window.title = "Yuri Settings"
+        window.contentViewController = viewController
+
+        return NSWindowController(window: window)
+    }
+
+    private func refreshPermissionState() {
+        let status = AccessibilityPermissionService.currentStatus()
+
+        permissionStatusMenuItem.title = status.menuTitle
+        openAccessibilitySettingsMenuItem.isHidden = status.isTrusted
+
+        statusItem?.button?.title = status.isTrusted ? "Yuri" : "Yuri!"
+        statusItem?.button?.toolTip = status.isTrusted
+            ? "Yuri"
+            : "Yuri needs Accessibility access"
+    }
+
+    private func debugShowSettingsOnLaunchIfNeeded() {
+        #if DEBUG
+            openSettings(nil)
+            NSLog("Yuri debug launch opened settings window.")
+        #endif
+    }
+
+    private func configureActivationPolicy() {
+        #if DEBUG
+            NSApp.setActivationPolicy(.regular)
+        #else
+            NSApp.setActivationPolicy(.accessory)
+        #endif
     }
 }
--- Yuri/ViewController.swift
+++ Yuri/ViewController.swift
@@ -7,4 +7,93 @@
 
 import Cocoa
 
-class ViewController: NSViewController {}
+final class ViewController: NSViewController {
+    private let titleLabel = NSTextField(labelWithString: "Accessibility Permission")
+    private let statusLabel = NSTextField(labelWithString: "")
+    private let detailLabel = NSTextField(wrappingLabelWithString: "")
+    private lazy var actionButton = makeActionButton()
+    private lazy var stackView = makeStackView()
+
+    override func loadView() {
+        view = NSView(frame: NSRect(x: 0, y: 0, width: 460, height: 260))
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        updatePermissionUI()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleDidBecomeActive(_:)),
+            name: NSApplication.didBecomeActiveNotification,
+            object: nil
+        )
+    }
+
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        updatePermissionUI()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc private func openAccessibilitySettings(_ sender: Any?) {
+        _ = AccessibilityPermissionService.requestPrompt()
+
+        guard AccessibilityPermissionService.openSystemSettings() else {
+            NSSound.beep()
+            return
+        }
+    }
+
+    @objc private func handleDidBecomeActive(_ notification: Notification) {
+        updatePermissionUI()
+    }
+
+    private func configureView() {
+        titleLabel.font = .systemFont(ofSize: 22, weight: .semibold)
+
+        statusLabel.font = .systemFont(ofSize: 13, weight: .medium)
+        detailLabel.textColor = .secondaryLabelColor
+        detailLabel.lineBreakMode = .byWordWrapping
+
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    private func updatePermissionUI() {
+        let status = AccessibilityPermissionService.currentStatus()
+
+        statusLabel.stringValue = status.settingsStatusText
+        statusLabel.textColor = status.isTrusted ? .systemGreen : .systemOrange
+        detailLabel.stringValue = status.settingsDetailText
+        actionButton.isHidden = status.isTrusted
+    }
+
+    private func makeActionButton() -> NSButton {
+        let button = NSButton(
+            title: "Open Accessibility Settings…",
+            target: self,
+            action: #selector(openAccessibilitySettings(_:))
+        )
+        button.bezelStyle = .rounded
+        return button
+    }
+
+    private func makeStackView() -> NSStackView {
+        let stackView = NSStackView(views: [titleLabel, statusLabel, detailLabel, actionButton])
+        stackView.alignment = .leading
+        stackView.orientation = .vertical
+        stackView.spacing = 12
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }
+}
